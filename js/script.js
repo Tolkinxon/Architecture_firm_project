bbf12a1a79a1a4b6f60bd0029979c2ded292c39a
Tolkinxon <krosstony639@gmail.com>
2023-01-18 00:06:27 +0500

    first commit

---
+++ js/script.js
@@ -0,0 +1,62 @@
+'use strict'
+
+const wrapperCarusel = document.querySelector('.carusel')
+const images = document.querySelectorAll('.slideImg')
+let slide = document.querySelector('.slide')
+const next = document.querySelector('.fa-forward')
+const prev = document.querySelector('.fa-backward')
+
+slide.style.width = images.length * 100 + '%'
+
+let width = window.getComputedStyle(wrapperCarusel).width //carusel classli divini eni uzunligi
+width = parseInt(width)
+
+wrapperCarusel.style.overflow = 'hidden'
+
+let offset = 0
+
+next.addEventListener('click', () => {
+  offset -= width
+
+  slide.style.transform = `translateX(${offset}px)`
+  slide.style.transition = '0.5s ease all'
+
+  if (offset === width * -4) {
+    setTimeout(() => {
+      slide.remove()
+
+      slide = document.createElement('div')
+      slide.classList.add('slide')
+      slide.style.width = images.length * 100 + '%'
+
+      images.forEach((item) => {
+        slide.append(item)
+      })
+      wrapperCarusel.append(slide)
+      offset = 0
+    }, 500)
+  }
+})
+
+prev.addEventListener('click', () => {
+  offset += width
+  slide.style.transform = `translateX(${offset}px)`
+  slide.style.transition = '0.5s ease all'
+
+  if (offset > 0) {
+    setTimeout(() => {
+      slide.remove()
+
+      slide = document.createElement('div')
+      offset = width * -3
+      slide.style.transform = `translateX(${offset}px)`
+      slide.classList.add('slide')
+      slide.style.width = images.length * 100 + '%'
+
+      images.forEach((item) => {
+        slide.append(item)
+      })
+      wrapperCarusel.append(slide)
+    }, 500)
+  }
+})
