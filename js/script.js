43018cf0c589292ab90cdc8c73eceed435ffa6e7
Tolkinxon <krosstony639@gmail.com>
2023-01-18 20:33:22 +0500

    some changes

--- js/script.js
+++ js/script.js
@@ -15,7 +15,39 @@ wrapperCarusel.style.overflow = 'hidden'
 
 let offset = 0
 
+const movingCarusel = setInterval(() => {
+  offset -= width
+
+  slide.style.transform = `translateX(${offset}px)`
+  slide.style.transition = '0.5s ease all'
+
+  if (offset === width * -4) {
+    setTimeout(() => {
+      slide.remove()
+
+      slide = document.createElement('div')
+      slide.classList.add('slide')
+      slide.style.width = images.length * 100 + '%'
+
+      images.forEach((item) => {
+        slide.append(item)
+      })
+      wrapperCarusel.append(slide)
+      offset = 0
+    }, 500)
+  }
+
+},1000)
+
+
+wrapperCarusel.addEventListener('mouseenter',() => {
+  clearInterval(movingCarusel)
+})
+
+
+
 next.addEventListener('click', () => {
+  clearInterval(movingCarusel)
   offset -= width
 
   slide.style.transform = `translateX(${offset}px)`
@@ -39,6 +71,8 @@ next.addEventListener('click', () => {
 })
 
 prev.addEventListener('click', () => {
+  clearInterval(movingCarusel)
+
   offset += width
   slide.style.transform = `translateX(${offset}px)`
   slide.style.transition = '0.5s ease all'
