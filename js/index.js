e4ea3c4d5035c09b67baa16e1df20913a025d0c1
Tolkinxon <krosstony639@gmail.com>
2023-05-04 19:08:27 +0500

    some changes

--- js/index.js
+++ js/index.js
@@ -1,23 +1,33 @@
 'use strict'
 
 const activeBtn = document.querySelector('.about')
+const itemClass = document.querySelectorAll('.item')
 const iframeSide = document.querySelector('iframe')
 
 activeBtn.addEventListener('click', (e) => {
-  console.log(e.target.className)
+  console.log(e.target.parentElement)
+
+  itemClass.forEach(item => { item.classList.remove('active')})
+
   if (e.target.className === 'home-page') {
+    e.target.parentElement.classList.add('active')
     iframeSide.src = 'indexHeader.html'
   }
   if (e.target.className === 'about-page') {
+    e.target.parentElement.classList.add('active')
     iframeSide.src = 'about.html'
   }
   if (e.target.className === 'commercial-page') {
+
+    e.target.parentElement.classList.add('active')
     iframeSide.src = 'commercial.html'
   }
   if (e.target.className === 'residential-page') {
+    e.target.parentElement.classList.add('active')
     iframeSide.src = 'residential.html'
   }
   if (e.target.className === 'contact-page') {
+    e.target.parentElement.classList.add('active')
     iframeSide.src = 'contact.html'
   }
 })
