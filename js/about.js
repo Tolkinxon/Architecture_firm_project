90b942b0863e6356369bfcd10cb4bd5fd25e5365
Tolkinxon <krosstony639@gmail.com>
2023-05-15 23:56:28 +0500

    taking width

--- js/about.js
+++ js/about.js
@@ -5,6 +5,10 @@ const containerEl = document.querySelector('.container')
 // const widthContainer = window.getComputedStyle(containerEl).width
 // console.log(widthContainer)
 
+containerEl.style.width = '900px'
+
 const widthContainer = containerEl.offsetWidth
+const heightContainer = containerEl.offsetHeight
 
 console.log(widthContainer)
+console.log(heightContainer)
